11ffbab43422c5c7ce5e5a99edcfcfb1fcedf2bd
doctorbilardo <g.derbes@paisanos.io>
2024-03-28 01:44:53 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "ignorePatterns": [".eslintrc.cjs", "dist", "vite.config.ts", "commitlint.config.cjs", ".prettierrc"],
+    "plugins": ["simple-import-sort"],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"), // el entry point es el punto de entrada
+      // de la app. Y a partir de el va armando un grafo de dependencias
+      name: "utils-lib",
+      fileName: "utils-lib",
+    },
+    sourcemap: true,
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "/src"),
+    },
+  },
+  plugins: [dts({ outDir: "dist", include: ["src/"] })],
+  // @ts-expect-error test types are not defined
+  test: {
+    globals: true,
+  },
+});
