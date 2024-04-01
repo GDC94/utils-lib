4ffa187b842195304fd675a909e56e0869265cb6
EXB32665 <german.derbescatoni@claro.com.ar>
2024-04-01 15:22:02 -0300

    chore: modified commit msg

--- src/check/getDifferenceObject.ts
+++ src/check/getDifferenceObject.ts
@@ -1,7 +1,7 @@
 type GenericObject = Record<string | number, unknown>
 
-function returnShortArray(arr1: string[], arr2: string[]): string[] {
-    return arr1.length > arr2.length ? arr1 : arr2
+function returnShortArray (arr1: string[], arr2: string[]): string[] {
+  return arr1.length > arr2.length ? arr1 : arr2
 }
 
 /**
@@ -13,34 +13,33 @@ function returnShortArray(arr1: string[], arr2: string[]): string[] {
  * @returns a record object that represents the difference between `object1` and `object2`.
  */
 
-
-export default function getDifferenceObject(
-    object1: GenericObject,
-    object2: GenericObject
+export default function getDifferenceObject (
+  object1: GenericObject,
+  object2: GenericObject
 ): Record<string, unknown> {
-    const differenceObj: Record<string, unknown> = {}
-    //    Get the keys of the object
-    const obj1Keys = Object.keys(object1)
-    const obj2Keys = Object.keys(object2)
+  const differenceObj: Record<string, unknown> = {}
+  //    Get the keys of the object
+  const obj1Keys = Object.keys(object1)
+  const obj2Keys = Object.keys(object2)
 
-    returnShortArray(obj1Keys, obj2Keys).forEach((key) => {
-        if (
-            object1[key] !== object2[key] &&
+  returnShortArray(obj1Keys, obj2Keys).forEach((key) => {
+    if (
+      object1[key] !== object2[key] &&
             typeof object1[key] === 'object' &&
             typeof object2[key] === 'object'
-        ) {
-            const diff = getDifferenceObject(
-                object1[key] as GenericObject,
-                object2[key] as GenericObject
-            )
-            if (Object.keys(diff).length > 0) {
-                differenceObj[key] = diff
-            }
-            return
-        }
-        if (object1[key] !== object2[key]) {
-            differenceObj[key] = object2[key]
-        }
-    })
-    return differenceObj
+    ) {
+      const diff = getDifferenceObject(
+        object1[key] as GenericObject,
+        object2[key] as GenericObject
+      )
+      if (Object.keys(diff).length > 0) {
+        differenceObj[key] = diff
+      }
+      return
+    }
+    if (object1[key] !== object2[key]) {
+      differenceObj[key] = object2[key]
+    }
+  })
+  return differenceObj
 }
