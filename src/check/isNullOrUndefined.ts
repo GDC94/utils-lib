d4ce08085edc72f2015cc897ca1e783933e8d908
EXB32665 <german.derbescatoni@claro.com.ar>
2024-04-01 18:09:33 -0300

    chore: adding unboxarr method

--- src/check/isNullOrUndefined.ts
+++ src/check/isNullOrUndefined.ts
@@ -1,5 +1,5 @@
 type NullOrUndefined = null | undefined
 
-export function isNullOrUndefined(value: unknown): value is NullOrUndefined {
-    return value === null || value === undefined
+export function isNullOrUndefined (value: unknown): value is NullOrUndefined {
+  return value === null || value === undefined
 }
