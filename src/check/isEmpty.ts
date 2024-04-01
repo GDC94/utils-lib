de8278828b20f66709df550f635475bfc7d2bdc4
EXB32665 <german.derbescatoni@claro.com.ar>
2024-04-01 15:18:50 -0300

    chore: styling commit msg

--- src/check/isEmpty.ts
+++ src/check/isEmpty.ts
@@ -1,6 +1,6 @@
 export function isEmpty<T> (object: T): boolean {
   const safeObject = object
   const isCountable = safeObject instanceof Object || Array.isArray(safeObject)
-  if (!isCountable) return true
+  if (!isCountable) { return true }
   return Object.entries(safeObject).length === 0
 }
