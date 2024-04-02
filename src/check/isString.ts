10dc8c00f1f23e8a039a77a7b0cf7695cb0e2dc4
EXB32665 <german.derbescatoni@claro.com.ar>
2024-04-02 16:11:18 -0300

    chore: adding vitest

--- src/check/isString.ts
+++ src/check/isString.ts
@@ -1,24 +1,24 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 // Definir el esquema para validar el tipo de valor
-const stringSchema = z.string();
+const stringSchema = z.string()
 
 /**
  * Checks if a value is a string.
- * 
+ *
  * @param {unknown} value - The value to check.
  * @returns {value is string} - True if the value is a string, false otherwise.
- * 
+ *
  * @example
  * // Example usage:
  * const value1 = 'hello';
  * const value2 = 42;
  * isString(value1); // Returns: true
  * isString(value2); // Returns: false
- * 
+ *
  * @note value is string | something is a way of telling the compiler that the function acts as a guard type.
  */
 
-export function isString(value: unknown): value is string {
-    return stringSchema.safeParse(value).success;
+export function isString (value: unknown): value is string {
+  return stringSchema.safeParse(value).success
 }
